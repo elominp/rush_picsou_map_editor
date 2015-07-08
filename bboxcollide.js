77248ccb17f778b286d868b8b983e0817c68a4ba
pirou_g <pirou_g@epitech.eu>
2015-07-08 09:53:03 +0200

    delete unused files

--- bboxcollide.js
+++
@@ -1,9 +0,0 @@
-function	box_collide(hero, monstre)
-{
-    if (monstre->x + monstre->width > hero->y &&
-	hero->x + hero->width > monstre->x &&
-	hero->y < monstre->y + monstre->height &&
-	hero->height + hero->y > monstre->y)
-	    return (true);
-    return (false);
-}
